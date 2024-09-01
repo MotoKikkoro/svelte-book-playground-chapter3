d8b6ae7d9844d7972f44a55f18ffe24914fc3f99
bergenryugaku@gmail.com <bergenryugaku@gmail.com>
2024-09-01 16:36:22 +0900

    プレフィックス 3.1.8

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,29 @@
 <script>
-  let todos =[];
-  function handleClick(){
-    todos = [...todos, ('新しいtodo')];
-}
+  let range = {min:0, max:5};
+  let values =[];
+
+  $: {
+    const newValues = [];
+    for (let i = range.min; i <= range.max; i++) {
+      newValues.push(i);
+    }
+    values = newValues;
+  }
+
+  function handleMinChange(event) {
+    range.min =parseInt(event.target.value);
+  }
+
+  function handleMaxChange(event){
+    range.max = parseInt(event.target.value);
+  }
 </script>
 
-<ul>
-  {#each todos as todo}
-  <li>{todo}</li>
-  {/each}
-</ul>
+<div>
+  <input type="number" min={0} max ={range.max} value={range.min} on:change={handleMinChange}>
+  <input type="number" min={range.min} max ={100} value={range.max} on:change={handleMaxChange}>
+</div>
 
-<button on:click={handleClick}>追加</button>
+{#each values as value}
+<button>value</button>
+{/each}
